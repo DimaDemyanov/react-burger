e7fb18e8780efec029a55694c9a16b935c1cd844
Dmitrii Demianov <dima.demyanov.1997@gmail.com>
2023-01-11 00:45:33 +0100

    Added IngredientDetails and OrderDetails and using API instead of mock data

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import './App.css';
 import AppHeader from './components/header/app-header';
 import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
 import BurgerConstructor from './components/burger-constructor/burger-constructor';
-import INGREDIENTS from './utils/data';
+import React, { useEffect } from 'react';
 
 function App() {
+  const [ingredients, setIngredients] = React.useState([]);
+
+  useEffect(() => {
+    fetch('https://norma.nomoreparties.space/api/ingredients')
+      .then(response => response.json())
+      .then(json => {
+        let ingredients = json.data;
+        ingredients[0].count = 1;
+        setIngredients(ingredients);
+      })
+      .catch(er => console.log(er))
+  }, []);
+
   return (
     <div className="App">
       <AppHeader />
       <div className='menu-container'>
-        <BurgerIngredients ingredients={INGREDIENTS} />
-        <BurgerConstructor ingredients={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(key => INGREDIENTS[key])} />
+        <BurgerIngredients ingredients={ingredients} />
+        <BurgerConstructor ingredients={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(key => ingredients[key]).filter(it => it !== undefined)} />
       </div>
     </div>
   );
--- src/components/header/app-header.jsx
+++ src/components/header/app-header.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const HeaderButton = ({ name, Icon, type, isActive = true }) => {
     let textColor = isActive ? "" : " text_color_inactive";
     return (
-        <div className={`${headerStyles.buttons} ${textColor} ml-2 mt-4 mb-4 text text_type_main-default`}>
+        <div className={`${headerStyles.button} ${textColor} ml-2 mt-4 mb-4 text text_type_main-default`}>
             <Icon className="ml-5" type={type} />
             <p className="ml-2 mr-5">{name}</p>
         </div>
@@ -15,19 +15,24 @@ const HeaderButton = ({ name, Icon, type, isActive = true }) => {
 HeaderButton.propTypes = {
     name: PropTypes.string.isRequired,
     type: PropTypes.oneOf(["primary", "secondary"]).isRequired,
+    Icon: PropTypes.elementType,
     isActive: PropTypes.bool
 };
 
 const AppHeader = () => {
     return (
         <div className={headerStyles.header}>
-            <div className={headerStyles.buttons}>
-                <HeaderButton name="Конструктор" Icon={BurgerIcon} type="primary" />
-                <HeaderButton name="Лента заказов" Icon={ListIcon} type="secondary" isActive={false} />
-            </div>
-            <Logo />
-            <div className={headerStyles.buttons}>
-                <HeaderButton name="Личный кабинет" Icon={ProfileIcon} type="secondary" isActive={false} />
+            <div className={headerStyles.nav}>
+                <div className={headerStyles.buttonsLeft}>
+                    <HeaderButton name="Конструктор" Icon={BurgerIcon} type="primary" />
+                    <HeaderButton name="Лента заказов" Icon={ListIcon} type="secondary" isActive={false} />
+                </div>
+                <div className={headerStyles.logo}>
+                    <Logo />
+                </div>
+                <div className={headerStyles.buttonsRight}>
+                    <HeaderButton name="Личный кабинет" Icon={ProfileIcon} type="secondary" isActive={false} />
+                </div>
             </div>
         </div>
     );
