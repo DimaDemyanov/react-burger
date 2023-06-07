b3df93391eec392d907647ce93ead5c6fc2e8464
Dmitrii Demianov <dima.demyanov.1997@gmail.com>
2023-06-07 02:00:09 +0200

    [sprint-6/step-1] - Application deployment script is added to package.json

---
+++ src/services/reducers/order.test.ts
@@ -0,0 +1,25 @@
+import { GET_ORDER_NUMBER_FAILED, GET_ORDER_NUMBER_SUCCESS } from "../actions/send-order";
+import { orderNumber } from "./order";
+
+describe("order reducer", () => {
+  it("should return the initial state", () => {
+    expect(orderNumber(undefined, { type: "none" })).toEqual("-");
+  });
+
+  it("should handle GET_ORDER_NUMBER_SUCCESS", () => {
+    expect(
+      orderNumber("-", {
+        type: GET_ORDER_NUMBER_SUCCESS,
+        orderNumber: 123,
+      })
+    ).toEqual("123");
+  });
+
+  it("should handle GET_ORDER_NUMBER_FAILED", () => {
+    expect(
+      orderNumber("-", {
+        type: GET_ORDER_NUMBER_FAILED,
+      })
+    ).toEqual("-");
+  });
+});
